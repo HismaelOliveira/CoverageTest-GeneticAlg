bb1d2e5613a46964bb4c9c1432e7e690a72c0f3e
RafaelCosta <rafael.costa@alu.ufc.br>
2019-06-11 14:39:33 -0300

    html print

--- TestCoverage/TestCoverage.py
+++ TestCoverage/TestCoverage.py
@@ -6,7 +6,7 @@ import re
 #Recebe um vetor com os valores das variaveis a serem testadas
 #retorna o percentual do cdg que foi coberto
 
-def test_arq(values):
+def test_arq(values, html= False):
     name_file = 'test_file.py'
     cmd = "coverage run --omit "+name_file+" "+name_file
 
@@ -21,10 +21,13 @@ def test_arq(values):
         stderr=subprocess.STDOUT,
         shell=True).decode('utf-8')
 
+    if html:
+        os.system("coverage html")
+
     test = var.split('\n')
 
     line = test[2]
     value = re.findall(r"[\w']+", line)[-1]
 
-    print(value)
+    #print(value)
     return value
